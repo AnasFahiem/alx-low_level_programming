eb695083ae80c0128d916a2a93323b786904b0aa
AnasFahiem <anasfahiem18@gmail.com>
2023-02-21 14:43:42 -0800

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,14 +11,14 @@ int print_sign(int n)
 	
 	if (n > 0)
 	{
-		t = 1
+		t = 1;
 		_putchar('+');
 	}else if (n == 0)
 	{
-		t = 0
+		t = 0;
 	        _putchar('0');
 	}else{
-		t = -1
+		t = -1;
 		_putchar('-');
 	}
 	return (t);
