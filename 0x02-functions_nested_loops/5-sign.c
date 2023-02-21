f9a974bc8860944b28d2ec6567d2c4bd708283e7
AnasFahiem <anasfahiem18@gmail.com>
2023-02-21 14:45:40 -0800

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,6 +2,7 @@
 
 /**
  * print_sign - prints the sign of a number
+ * @n : integer for check
  * Return: 0 or 1 or -1
  */
 
@@ -13,11 +14,14 @@ int print_sign(int n)
 	{
 		t = 1;
 		_putchar('+');
-	}else if (n == 0)
+	}
+	else if (n == 0)
 	{
 		t = 0;
 	        _putchar('0');
-	}else{
+	}
+	else
+	{
 		t = -1;
 		_putchar('-');
 	}
