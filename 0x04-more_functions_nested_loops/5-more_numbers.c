9afab6d1e8c04ae4628530b9a9573d9f0840528d
AnasFahiem <anasfahiem18@gmail.com>
2023-02-23 11:00:37 -0800

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * more_numbers - prints 10 times the numbers
+ * Return: 0
+ */
+
+void more_numbers(void)
+{
+	for (i = 0; i <= 10; i++)
+	{
+		char c = 0;
+
+		while (c <= 14)
+		{
+			_putchar('0' + c);
+			c++;
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
