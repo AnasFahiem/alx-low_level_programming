408a42a810eb5e4deffc24a6a4c8070e27776044
AnasFahiem <anasfahiem18@gmail.com>
2023-03-01 07:12:52 -0800

    bbb

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -6,23 +6,24 @@
  * Return: s transformed
  */
 
-char *leet(char *)
+char *leet(char *s)
 {
 	int i, ii;
 	char s1[] = "aeotl";
-	char s2[] = "AEOTL";
-	char s3[] = "43071";
+	char S1[] = "AEOTL";
+	char s2[] = "43071";
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (ii = 0; ii < 5; ii++)
 		{
-			if (s[i] == s1[ii] || s[i] == s2[ii])
+			if (s[i] == s1[ii] || s[i] == S1[ii])
 			{
-				s[i] = s3[ii];
+				s[i] = s2[ii];
 				break;
 			}
 		}
 	}
 	return (s);
 }
+
