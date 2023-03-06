02b78109661837722e4083862462831145b884e0
AnasFahiem <anasfahiem18@gmail.com>
2023-03-06 11:06:24 -0800

    5-strstr.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -12,7 +12,7 @@ char *_strpbrk(char *s, char *accept)
 {
 	int i;
 
-	while(*s)
+	while (*s)
 	{
 		for (i = 0; accept[i]; i++)
 		{
@@ -25,3 +25,4 @@ char *_strpbrk(char *s, char *accept)
 	}
 	return (NULL);
 }
+
