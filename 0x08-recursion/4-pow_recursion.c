b6f53b6a994561cbeacd38a7e218d6434817271e
AnasFahiem <anasfahiem18@gmail.com>
2023-03-08 15:58:53 -0800

    4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -11,16 +11,11 @@
 int _pow_recursion(int x, int y)
 {
 	if (y < 0)
-	{
 		return (-1);
-	}
-	if else (y == 0)
-	{
+	else if (y == 0)
 		return (1);
-	}
-	if else (y == 1)
-	{
+	else if (y == 1)
 		return (x);
-	}
+
 	return (x * _pow_recursion(x, y - 1));
 }
